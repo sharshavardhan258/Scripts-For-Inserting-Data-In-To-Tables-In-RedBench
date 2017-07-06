b40acac932e5764a5debfc4d81dd8ee8cea777c6
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-06 17:19:54 +0530

    Create Organization.sql

---
+++ Organization.sql
@@ -0,0 +1,16 @@
+INSERT INTO [dbo].[Org]
+           ([First Name]
+           ,[Last Name]
+           ,[Email]           
+           ,[Password]
+           ,[Contact Number]         
+           ,[Gender]           
+           ,[Address line 1]       
+           ,[City]
+           ,[State]
+           ,[Zip Code]          
+           ,[Type]
+         )
+     VALUES     
+ ('Bhaktavastal','Reddy','bhaktavatsal.nedurumalli@gmail.com','nellore123',919493236665,'Male','Illinoise','Chicago','Chicago',50006,'Organization')
+GO
