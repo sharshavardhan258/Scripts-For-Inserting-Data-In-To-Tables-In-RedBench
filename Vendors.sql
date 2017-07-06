e24af37f52d71bf571e3c63884e92bf6856f56a6
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-06 17:22:08 +0530

    Create Vendors.sql

---
+++ Vendors.sql
@@ -0,0 +1,22 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Vendors]
+           ([Name]
+           ,[Address 1]           
+           ,[City]
+           ,[State]
+           ,[Country]
+           ,[Zip]
+           ,[Website]
+           ,[Tags]
+           ,[Phone]          
+           ,[Email]          
+           ,[OrganizationID]
+           ,[Password]          
+           )
+     VALUES
+           ('TreeGoSys','Hightech City','Hyderabad','Telangana','INDIA',500072,
+		   'www.treegosystems.com','Engineering',+9493236665,'sharshavardhan258@gmail.com',103131,'harsha')
+		  
+GO
