aceae36347d305d9a9e276598c08f32a1abdf722
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-06 17:22:43 +0530

    Create Contact and Address.sql

---
+++ Contact and Address.sql
@@ -0,0 +1,13 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[ContactandAddress]
+           ([ContactName]
+           ,[JobPosition]
+           ,[Email]           
+           ,[Phone]
+           ,[VendorID])
+     VALUES
+           
+('Sujith','AWS Admin','sujith.reddy2014@gmail.com',+917032634261,200200)
+GO
