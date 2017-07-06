e51c44f40edba844f46697fb6af6dfd2ce8f9495
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-06 17:31:31 +0530

    Create Workforce For On Bench.sql

---
+++ Workforce For On Bench.sql
@@ -0,0 +1,29 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[WorkForce]
+           ([VendorID]
+           ,[First Name]
+           ,[Middle Name]
+           ,[Last Name]
+           ,[Gender]
+           ,[Email Address]        
+           ,[Contact Number]         
+           ,[Address 1]     
+           ,[City]
+           ,[State]
+           ,[ZipCode]
+           ,[JobtitleId]          
+           ,[Experience]          
+           ,[Current Job Status]
+           ,[Prefered Hourly Rate]
+           ,[Maximum Hourly Rate]
+           ,[OrganizationID]
+           ,[TechnicalSkills]
+           ,[Bench]
+          )
+     VALUES
+           (200200,'Lokesh','LR','Reddy','Male','swaroop1232010@gmail.com',
+		   +918501020553,'Hightech City','Hyderabad','Telangana',500072,104000,3,'Actively Seeking',50,65,103131,106002,1)
+		  
+GO
