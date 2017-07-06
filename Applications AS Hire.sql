c960f481d6acb99ec70594dd596ca5b7e5372f52
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-06 17:27:29 +0530

    Create Applications AS Hire.sql

---
+++ Applications AS Hire.sql
@@ -0,0 +1,14 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Applications]
+           ([WorkForce]
+           ,[JobPositions]
+           ,[Organization]
+           ,[Vendor]
+           ,[ApplicationStatus]
+           ,[AppliedOn])
+     VALUES
+           (300510,300075,103131,200200,'Apply','2017-07-05')
+		   
+GO
