2387a6b25743ee9065360e648642af1767c4e3bd
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-06 17:20:38 +0530

    Create Company.sql

---
+++ Company.sql
@@ -0,0 +1,20 @@
+USE [RedBenchDB]
+GO
+
+INSERT INTO [dbo].[Company]
+           ([Name]
+           ,[Website]
+           ,[Industry]
+           ,[Size]
+           ,[Revenue]
+           ,[Headquarters]        
+           ,[Address1]
+           ,[City]
+           ,[State]
+           ,[ZipCode]
+           ,[OrganizationID])
+     VALUES
+          
+		   ('Ensar','http://www.ensarsolutions.com/','Software',100,100000,'US','Illinoise','Chicago','Chicago',50006, 103131)
+GO
+
